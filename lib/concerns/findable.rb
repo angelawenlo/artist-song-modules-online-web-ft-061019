e41434f1d59f7ba17d1da551c0631e46a6385396
Angela Lo <angelawenlo@gmail.com>
2019-06-28 19:05:21 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,3 +1,10 @@
 module Findable
+  #
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
+  def self.find_by_name(name)
+    all.detect{|a| a.name == name}
+  end
 end
