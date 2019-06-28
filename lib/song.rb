0a2a654d02220abbf82e9336bb8116025aa1fcf6
Angela Lo <angelawenlo@gmail.com>
2019-06-28 19:08:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable
+    include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -23,7 +24,7 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-  # 
+  #
   # def to_param
   #   name.downcase.gsub(' ', '-')
   # end
